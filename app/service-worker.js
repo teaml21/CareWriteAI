3e42f0ffea4465bee2c63ebce4adb7fe868bb77d
teaml21 <lauram201914@icloud.com>
2026-09-13 15:26:17 +0100

    Add service worker for caching app files

---
+++ app/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'carewriteai-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
